c0efd0297154e121b95dc314824fac435d7e3db9
RashmiJK <rashmi.kare@gmail.com>
2025-02-27 20:33:37 +0000

    add search function

--- src/app.py
+++ src/app.py
@@ -1,9 +1,10 @@
 from typing import Annotated
 
-import requests
 from fastapi import FastAPI, Form, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+
+from src.git_search import search_github_repositories
 
 app = FastAPI()
 
@@ -13,15 +14,17 @@ templates = Jinja2Templates(directory="src/templates")
 
 @app.get("/", response_class=HTMLResponse)
 def get_home(request: Request):
-    return templates.TemplateResponse("home.html", {"request": request})
+    error = request.query_params.get("error")
+    return templates.TemplateResponse("home.html", {"request": request, "error": error})
 
 
 @app.post("/search_results", response_class=HTMLResponse)
 def get_results(request: Request, search_keywords: Annotated[str, Form(...)]):
+    if not search_keywords:
+        return templates.TemplateResponse("home.html", {"request": request, "error": "Please enter some keywords."})
     print(f"search_keywords: {search_keywords}")
-    # Call GitHub API to search for repositories
-    response = requests.get(f"https://api.github.com/search/repositories?q={search_keywords}")
-    search_results = response.json().get("items", [])
+    # issue search request
+    search_results = search_github_repositories(search_keywords)
     return templates.TemplateResponse(
         "results.html", {"request": request, "search_keywords": search_keywords, "search_results": search_results}
     )
